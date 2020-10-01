cf4997b2c0b7bd1be8ee6aede9243b28779cabdf
Nathán <n.mtz.cas@gmail.com>
2020-10-01 16:24:48 -0500

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile
+from profiles_api import models
 
-admin.site.register(UserProfile)
+admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 
 
 # Register your models here.
